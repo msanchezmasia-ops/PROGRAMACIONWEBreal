eef0359162fe041c42fd689ab681a2261d9abd66
Matias Sanchez <msanchezmasia@itba.edu.ar>
2026-04-28 19:33:18 -0300

    Cambio a React y semantica de HTML

---
+++ Pagina_web/src/index.js
@@ -0,0 +1,9 @@
+// Punto de entrada de la aplicación React
+// Este archivo conecta React con el index.html
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './script';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
