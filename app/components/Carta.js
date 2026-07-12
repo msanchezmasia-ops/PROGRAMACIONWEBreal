c52a33b4d176e7c8366aefe0c83af5d4262e1d95
Matias Sanchez <msanchezmasia@itba.edu.ar>
2026-07-11 21:09:38 -0300

    Cambios en como se visualizan los pedidos

--- app/components/Carta.js
+++ app/components/Carta.js
@@ -195,6 +195,7 @@ export default function Carta({ carta }) {
                 .select('*')
                 .eq('email', email)
                 .eq('pagado', true)
+                .eq('entregado', false)
                 .order('created_at', { ascending: false });
 
             if (error) throw error;
